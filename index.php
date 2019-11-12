9a969703d838962b2ab8686a9d7e3bb5e1b0b0ad
caedme2003 <57332556+caedme2003@users.noreply.github.com>
2019-11-11 23:37:06 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<body>
+
+<h1> mi web</h1>
+
+<?php
+include 'Calculadora.php';
+//  llamamos archivo Taza de cambio
+include 'Taza.php';
+//  llamamos archivo nombre
+
+include 'nombre.php';
+//  llamamos archivo arrayspays
+
+include 'arraypais.php';
+//  llamamos archivo agenda
+include 'agenda.php';
+
+
+?>
+
+</body>
+</html>
+
+
+</body>
+</html>
